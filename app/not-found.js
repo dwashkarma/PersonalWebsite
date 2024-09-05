06dacf19e32205e7710df3629f92eb69e095ab0b
DwashKarma <dwaskc@gmail.com>
2024-09-05 23:18:24 +0545

    added arial label

--- app/not-found.js
+++ app/not-found.js
@@ -5,7 +5,11 @@ export default function NotFound() {
     <div className="grid gap-4 place-content-center text-center h-[90dvh]">
       <h2 className="text-6xl font-bold text-textPrimary">Page Not Found</h2>
       <p className="font-light">Could not find requested resource</p>
-      <Link href="/" className="underline text-textPrimary">
+      <Link
+        href="/"
+        aria-label="homepage redirects"
+        className="underline text-textPrimary"
+      >
         Return Home
       </Link>
     </div>
