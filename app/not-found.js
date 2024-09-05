d63fd3918244a6b29728a101652434a336281459
DwashKarma <dwaskc@gmail.com>
2024-09-05 23:17:47 +0545

    404 page

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="grid gap-4 place-content-center text-center h-[90dvh]">
+      <h2 className="text-6xl font-bold text-textPrimary">Page Not Found</h2>
+      <p className="font-light">Could not find requested resource</p>
+      <Link href="/" className="underline text-textPrimary">
+        Return Home
+      </Link>
+    </div>
+  );
+}
