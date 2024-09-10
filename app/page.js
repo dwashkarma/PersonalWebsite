3964990148633c9eeca26ba37e99d57e0760bb4b
DwashKarma <dwaskc@gmail.com>
2024-09-10 16:20:04 +0545

    deleted one person schema

--- app/page.js
+++ app/page.js
@@ -2,19 +2,8 @@ import About from "@/components/About";
 import HeroSection from "@/components/HeroSection";
 import Skills from "@/components/Skills";
 export default function Home() {
-  const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    name: "Diwash Karmacharya",
-    image: "/dwas.jpg",
-    description: " Hi, I'm Diwash Karmacharya |Frontend Developer ",
-  };
   return (
     <div>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      ></script>
       <main>
         <HeroSection />
       </main>
