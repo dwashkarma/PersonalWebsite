e79b26cba4ea6e6d83008c367366e57fa31e0a1f
DwashKarma <dwaskc@gmail.com>
2024-09-10 15:16:05 +0545

    added url icon

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/dwas.jpg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+
         <Metrics />
       </head>
       <body className={poppins.className}>
