4c4fb9ee68944b2e9932baff3b188ba9f5b77d1d
DwashKarma <dwaskc@gmail.com>
2024-09-10 13:22:49 +0545

    refactor: sitemap and heading

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -51,7 +51,7 @@ function NavBar() {
             className="rounded-full "
           />
         </div>
-        <h2 className="text-xl md:hidden">Dwash</h2>
+        <p className="text-xl md:hidden">Dwash</p>
         <div
           className="flex gap-2 items-center md:hidden text-xl "
           onClick={handleMenuOpen}
